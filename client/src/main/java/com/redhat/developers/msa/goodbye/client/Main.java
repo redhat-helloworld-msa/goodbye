86a783ebed74728de2250bedad0c99fd6b7829f5
Burr Sutter <bsutter@redhat.com>
2017-01-19 14:39:48 -0500

    100 threads for Boot and Vert.x

--- client/src/main/java/com/redhat/developers/msa/goodbye/client/Main.java
+++ client/src/main/java/com/redhat/developers/msa/goodbye/client/Main.java
@@ -25,7 +25,7 @@ public class Main {
         for (int x = 0; x < NUMBER_THREADS; x++) {
             Thread.sleep(20);
             // new ApacheClientGoodbye().start();
-             new ApacheClient().start();
+            new ApacheClient().start();
             // new TimeoutApacheClient().start();
             // new HystrixClient().start();
         }
